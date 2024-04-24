d982669b56f641a05e5f68151c409e2737dacdd7
Daniel McHugh <daniel.mchugh@nymoen.no>
2024-04-24 16:08:49 +0200

    Added toastify to CreateCrib

--- src/Pages/CreateCrib.jsx
+++ src/Pages/CreateCrib.jsx
@@ -8,6 +8,7 @@ import api from '../api';
 import Cookies from "js-cookie";
 import ParseJwt from "./parseJwt";
 import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
 
 function CreateCrib() {
 
@@ -26,7 +27,7 @@ function CreateCrib() {
 
     const joinCribMutation = useMutation({
         mutationFn: (cribIdValue) => api.post(`/customer/${payload.customerId}/join/${cribIdValue}`),
-        onSuccess: () => {
+        onSuccess: (response) => {
             console.log('Successfully joined crib:', response.data);
         },
         onError: (error) => {
@@ -49,8 +50,13 @@ function CreateCrib() {
         }
     });
 
-    function createCribButton(){
-        createCribMutation.mutate();
+    function createCribButton() {
+        if (!cribNameValue) {
+            toast.error("Please add Crib name.")
+        } 
+        else {
+            createCribMutation.mutate();
+        }
     }
 
 
