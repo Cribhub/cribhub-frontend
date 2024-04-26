c09774db0c765f1b4ea4a8311ec6260b9bda893e
Marius Hatland <marius.hatland@student.uib.no>
2024-04-26 13:57:23 +0200

    Styled header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,72 +1,96 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import Logo from '../../Logo_GreyBg.PNG';
-import api from '../../api';
-import { useMutation } from '@tanstack/react-query';
-import { toast } from 'react-toastify';
+import React from 'react'
+import { styled } from '@mui/material/styles'
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    IconButton,
+    ListItemAvatar,
+} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import Logo from '../../Logo_GreyBg.PNG'
+import api from '../../api'
+import { useMutation } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import Badge from '@mui/material/Badge'
+import MailIcon from '@mui/icons-material/Mail'
+import Avatar from '@mui/material/Avatar'
+import { deepOrange } from '@mui/material/colors'
+import { Box, Drawer } from '@mui/material'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import theme from '../../theme'
 
 const StyledAppBar = styled(AppBar)({
     flexGrow: 1,
-});
+})
 
 const StyledToolbar = styled(Toolbar)({
     display: 'flex',
     justifyContent: 'space-between',
-});
+})
 
 const LeftContent = styled('div')({
     display: 'flex',
     alignItems: 'center',
-});
+})
 
 const RightContent = styled('div')({
     display: 'flex',
     alignItems: 'center',
-});
+})
 
 const StyledTypography = styled(Typography)({
     marginLeft: '8px', // Add left margin for spacing between items
-});
+})
 
 const StyledLogoutButton = styled(Button)({
     marginLeft: '8px', // Add left margin for spacing between items
-});
+})
 
 const StyledLeaveCribButton = styled(Button)({
     marginLeft: '8px', // Add left margin for spacing between items
-});
+})
 
 const StyledImage = styled('img')({
     width: 100,
     height: 100,
-});
+})
 
 const Header = ({ userName, crib, cribname, userID }) => {
-    const navigate = useNavigate();
+    const [open, setOpen] = React.useState(false)
+
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen)
+    }
+
+    const navigate = useNavigate()
 
     const leaveCribMutation = useMutation({
         mutationFn: () => api.post(`customer/${crib}/leave/${userID}`),
         onSuccess: (response) => {
-            console.log("Left Crib: ", response)
+            console.log('Left Crib: ', response)
             navigate('/joinCrib')
         },
         onError: (error) => {
-            toast.error("Could not leave Crib!")
-            console.error("Error leaving crib: ", error)
-        }
-    });
+            toast.error('Could not leave Crib!')
+            console.error('Error leaving crib: ', error)
+        },
+    })
 
     const leaveCribOnclick = () => {
-        leaveCribMutation.mutate();
-    };
+        leaveCribMutation.mutate()
+    }
 
     const logout = () => {
-        navigate('/');
-        Cookies.remove('Token', '');
-    };
+        navigate('/')
+        Cookies.remove('Token', '')
+    }
 
     return (
         <StyledAppBar position="static">
@@ -75,27 +99,74 @@ const Header = ({ userName, crib, cribname, userID }) => {
                     <IconButton edge="start" color="inherit" aria-label="menu">
                         <StyledImage src={Logo} alt="My Image" />
                     </IconButton>
-                    <Typography variant="h6">
-                        Your crib: {crib}
-                    </Typography>
-                    <StyledLeaveCribButton color="inherit" onClick={leaveCribOnclick}>
-                        Leave Crib
-                    </StyledLeaveCribButton>
+                    <Box>
+                        <Typography variant="h6">{cribname}</Typography>
+                        <Typography>Crib Id: {crib}</Typography>
+                    </Box>
                 </LeftContent>
                 <RightContent>
-                    <Typography variant="h6">
-                        Logged in as: {userName}
-                    </Typography>
-                    <StyledTypography variant="h6">
-                        Your crib name: {cribname}
-                    </StyledTypography>
-                    <StyledLogoutButton color="inherit" onClick={logout}>
-                        Logout
-                    </StyledLogoutButton>
+                    <Box display="flex" alignItems="center" gap={2}>
+                        <Badge badgeContent={4} color="error">
+                            <MailIcon color="action" />
+                        </Badge>
+                        <Avatar
+                            onClick={toggleDrawer(true)}
+                            sx={{ bgcolor: deepOrange[500] }}
+                        >
+                            MA
+                        </Avatar>
+                    </Box>
                 </RightContent>
             </StyledToolbar>
+            <Drawer open={open} anchor="right" onClose={toggleDrawer(false)}>
+                <Box
+                    sx={{
+                        width: 350,
+                        height: '100%',
+                        backgroundColor: '#f0fff0',
+                    }}
+                >
+                    <List>
+                        <ListItem>
+                            <Typography variant="h4">Logged in as:</Typography>
+                        </ListItem>
+                        <ListItem>
+                            <Box display="flex" gap={2} justifyContent="center">
+                                <Typography variant="h4">{userName}</Typography>
+                                <Avatar
+                                    sx={{
+                                        height: '50px',
+                                        width: '50px',
+                                        bgcolor: deepOrange[500],
+                                    }}
+                                >
+                                    MA
+                                </Avatar>
+                            </Box>
+                        </ListItem>
+                        <ListItem>
+                            <Box display="flex" gap={2} justifyContent="center">
+                                <Button
+                                    onClick={logout}
+                                    variant="contained"
+                                    color="error"
+                                >
+                                    Logout
+                                </Button>
+                                <Button
+                                    onClick={leaveCribOnclick}
+                                    variant="contained"
+                                    color="error"
+                                >
+                                    Leave Crib
+                                </Button>
+                            </Box>
+                        </ListItem>
+                    </List>
+                </Box>
+            </Drawer>
         </StyledAppBar>
-    );
-};
+    )
+}
 
-export default Header;
+export default Header
